8a393b5fab192a8d75e8cd0e85b4ebd60fed25dd
Himanshu Patel <Himanshu.Patel.26@gmail.com>
2022-03-23 10:11:13 -0700

    Binary Search Modifications

--- src/main/java/com/patel/dsa/search/BinarySearch.java
+++ src/main/java/com/patel/dsa/search/BinarySearch.java
@@ -3,12 +3,24 @@ package com.patel.dsa.search;
 public class BinarySearch {
     public static void main(String[] args) {
         int arr[] = { 2, 4, 5, 6, 7, 18, 19, 70 }; // sorted array. Note binary search only works with sorted
-        int target = 18; // this is the element that we need to find
+        int target = 18; // this is the element need to find
         System.out.println("Binary Search: " + binarySearch(arr, target));
     }
 
-    private static String binarySearch(int[] arr, int target) {
-        return null;
+    private static int binarySearch(int[] arr, int target) {
+        int lowerbound = 0;
+        int upperbound = arr.length - 1;
+        int midpoint = -1;
+        while (lowerbound <= upperbound) {
+            midpoint = (lowerbound + upperbound) / 2;
+            if (arr[midpoint] == target) {
+                return target;
+
+            } else {
+
+            }
+        }
+        return -1;
     }
 
 }

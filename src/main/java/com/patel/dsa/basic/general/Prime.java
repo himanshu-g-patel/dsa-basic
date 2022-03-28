99403aa43d1642498ce3bb9b11e0ff3c1f288903
Himanshu Patel <Himanshu.Patel.26@gmail.com>
2022-03-27 23:52:45 -0700

    Added Loop execution counter

--- src/main/java/com/patel/dsa/basic/general/Prime.java
+++ src/main/java/com/patel/dsa/basic/general/Prime.java
@@ -3,7 +3,10 @@ package com.patel.dsa.basic.general;
 public class Prime {
     // Prime number is only divisible by 1 and itself
     static boolean isPrime(int num) {
+        int counter = 1;
         for (int i = 2; i < num; i++) {
+            System.out.println("Loop execution counter:  " + counter);
+            counter++;
             if (num % i == 0) {
                 return false;
             }
--- src/test/java/com/patel/dsa/basic/general/PrimeTest.java
+++ src/test/java/com/patel/dsa/basic/general/PrimeTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class PrimeTest {
     @Test
     void testIsPrime() {
-        assertTrue(Prime.isPrime(7));
+        assertTrue(Prime.isPrime(101));
     }
 
     @Test
